c352202a202038e9b96a029dfbbe22b8873e978b
NesyaKurnia <50094449+NesyaKurnia@users.noreply.github.com>
2021-01-30 17:33:40 +0800

    Add files via upload

---
+++ code.ino
@@ -0,0 +1,25 @@
+int rainDetector = A0;
+int sensorReading = 0;
+int buzzer = 10;
+int led = 11;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(rainDetector, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  sensorReading = analogRead(rainDetector);
+  Serial.println(sensorReading);
+  if(sensorReading < 500){
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(led, HIGH);
+  }
+  else{
+    digitalWrite(buzzer, LOW);
+    digitalWrite(led, LOW);
+  }
+  delay(1000);
+}
